3bd97fdcc501740a0c26b976db501c203e9f25d9
Dominic <otienodominic@gmail.com>
2020-09-19 20:27:28 +0300

    for my love Wambura

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { ThemeProvider } from "styled-components";
+import { useOnClickOutside } from "./hooks";
+import { GlobalStyles } from "./global";
+import { theme } from "./theme";
+import { Burger, Menu } from "./components";
+import FocusLock from "react-focus-lock";
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  const menuId = "main-menu";
+
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div ref={node}>
+          <FocusLock disabled={!open}>
+            <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+            <Menu open={open} setOpen={setOpen} id={menuId} />
+          </FocusLock>
+        </div>
+        <div>
+          <h1>Hello Purity Wambura My wife!. I love you very much Honey</h1>
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/148/148836.svg"
+            alt="Love icon"
+          />
+          <small>This is how much I love you honey</small>
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
