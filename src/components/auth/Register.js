0d72775f5d319535f5ff52d7ca64ece78f8501c3
Dominic <otienodominic@gmail.com>
2020-09-26 08:46:59 +0300

    making the authorisation

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,21 +1,79 @@
-import React from "react";
-
+import React, { useState, useContext } from "react";
+import Axios from "axios";
+import { useHistory } from "react-router-dom";
+import UserContext from "../../context/UserContext";
 export default function Register() {
+  const [firstname, setFirstname] = useState();
+  const [lastname, setLastname] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordCheck, setPasswordCheck] = useState();
+  const [username, setUsername] = useState();
+  const { setUserData } = useContext(UserContext);
+  const history = useHistory();
+  // Create a function that submits a form
+  const submit = async (e) => {
+    e.preventDefault();
+    const newUser = {
+      firstname,
+      lastname,
+      username,
+      email,
+      password,
+      passwordCheck,
+    };
+
+    await Axios.post(
+      "https://traffic-check.herokuapp.com/api/v1/register",
+      newUser
+    );
+
+    console.log(newUser)
+  };
+
   return (
     <div className="page">
       <h2>Register</h2>
-      <form>
+      <form onSubmit={submit}>
+        <label htmlFor="register-firstname">First Name</label>
+        <input
+          id="register-firstname"
+          type="text"
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+        <label htmlFor="register-lastname">Last Name</label>
+        <input
+          id="register-lastname"
+          type="text"
+          onChange={(e) => setLastname(e.target.value)}
+        />
+        <label htmlFor="register-username">Username</label>
+        <input
+          id="register-username"
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <label htmlFor="register-email">Email</label>
-        <input id="register-email" type="email" />
-
+        <input
+          id="register-email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label htmlFor="register-password">password</label>
-        <input id="register-password" type="password" />
-        <input id="password" placeholder="verify password" />
-    
-        <label htmlFor="register-display-name">Display Name</label>
-        <input id="register-display-name" type="text" />
+        <input
+          id="register-password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          id="password"
+          placeholder="verify password"
+          onChange={(e) => setPasswordCheck(e.target.value)}
+        />
 
-        <input></input>
+        <button type="submit" onClick={submit}>
+          Register
+        </button>
       </form>
     </div>
   );
